a306e6e8bb2cc8fd6277ebf8e32daf86cdbf93d0
runsvend <runar.svendsen@finn.no>
2015-12-15 23:00:09 +0100

    Added page 10 Process

---
+++ BreadMaker.playground/Pages/10 - Process.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+/*:
+## Process
+* Autolyse (flour + water)
+* Develop (add yeast + salt)
+* Bulk fermentation
+    * Stretch-and-fold 1
+    * Stretch-and-fold 2
+    * Stretch-and-fold 3
+* Divide, pre-shape
+* Final shape
+* Proof
+* Bake
+* Cool
+ 
+*/
+
+//: [Next](@next)
