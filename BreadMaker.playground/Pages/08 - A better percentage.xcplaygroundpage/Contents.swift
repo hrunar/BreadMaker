1dc17ced70f1dcd596f41af10b50d69135cf8fc6
runsvend <runar.svendsen@finn.no>
2015-12-15 14:03:04 +0100

    Added pages 7-9, postfix operator

---
+++ BreadMaker.playground/Pages/08 - A better percentage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+//: [Previous](@previous)
+
+//: ## Ingredients calculated! What if...?
+struct Dough {
+    
+    let yield: Double
+    let hydration: Double
+    
+    var flour: Double {
+        get {
+            return 100 * yield / (100 + hydration)
+        }
+    }
+    
+    var water: Double {
+        get {
+            return yield - flour
+        }
+    }
+    
+    var yeast: Double {
+        get {
+            return flour * 4%
+        }
+    }
+    
+    var salt: Double {
+        get {
+            return flour * 1.9%
+        }
+    }
+    
+    init(yield: Double, hydrationPercentage hydration: Double = 70.0) {
+        self.yield = yield
+        self.hydration = hydration
+    }
+    
+}
+
+let dough = Dough(yield: 1700)
+dough.yield
+dough.flour
+dough.water
+dough.yeast
+dough.salt
+
+//: [Next](@next)
