b9a60cedbf1f356542000a5c8b264b1d8da3525e
runsvend <runar.svendsen@finn.no>
2015-12-15 13:27:41 +0100

    Add page 4

---
+++ BreadMaker.playground/Pages/04 - Calculate ingredients 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+
+struct Dough {
+    
+    let yield: Double
+    let hydration: Double
+    
+    var flour: Double {
+        get {
+            return 100 * yield / (100 + hydration)
+        }
+    }
+
+/*: Todo: Calculate water weight */
+    
+    init(yield: Double, hydrationPercentage hydration: Double = 70.0) {
+        self.yield = yield
+        self.hydration = hydration
+    }
+}
+
+let dough = Dough(yield: 1700)
+dough.yield
+dough.flour
+dough.water
+
+//: [Next](@next)
+
+
